5303c6af1195045182d5746539f16817e0a1b756
ThuTrangNguyen92 <trangtulgu@gmail.com>
2023-10-15 19:02:10 +0700

    startFromZero

--- routes/indexRouter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/indexController');
-
-router.get('/',controller.showHomepage);
-router.get('/:page',controller.showPage);
-module.exports = router;
