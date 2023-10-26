640641509c65d7f105d4d318a4e5890574d09108
ThuTrangNguyen92 <trangtulgu@gmail.com>
2023-10-26 11:13:51 +0700

    HandlerbarsLoginSignUpPassenger

--- index.js
+++ index.js
@@ -2,12 +2,34 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5001;
+const expressHandlebars = require('express-handlebars');
 
 //cau hinh public static folder
 app.use(express.static(__dirname+'/public'))
 
+app.engine('hbs',expressHandlebars.engine({
+    layoutDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    defaultLayout:'layout',
+    //handlebar co the lay thuoc tinh tu trong CSDL - true
+    runtimeOptions: {allowProtoPropertiesByDefault: true},
+    helpers: {
+        /* createStarList,
+        createPagination */
+    },
+    
+}));
+app.set('view engine','hbs');
+
+
+
+
 app.get('/',(req,res)=>{
-    res.send('hello');
+    res.render('index');
+})
+app.get('/:page',(req,res)=>{
+    res.render(req.params.page);
 })
 
 app.listen(port,(req,res)=>{
